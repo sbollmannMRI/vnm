2f4284804f818e93a33b1cae94c30d8e4c5a788e
Steffen <stebo85@web.de>
2020-06-22 10:41:38 +1000

    added shutdown via ENTER option

--- build_run_stop.bat
+++ build_run_stop.bat
@@ -1,8 +1,7 @@
 docker build -t vnm:latest .
-REM docker run --privileged -e USER=neuro -e PASSWORD=neuro --name vnm -v C:/vnm:/vnm -p 6080:80 vnm:latest
 ECHO "Starting VNM:"
+REM docker run --privileged -e USER=neuro -e PASSWORD=neuro --name vnm -v C:/vnm:/vnm -p 6080:80 vnm:latest
 docker run -d --privileged -e USER=neuro -e PASSWORD=neuro --name vnm -v C:/vnm:/vnm -e RESOLUTION=1920x980 -p 6080:80 -p 5900:5900 vnm:latest
-ECHO "VNM is running - press any key to shutdown and quit VNM"
-PAUSE 
+set /p=VNM is running - press ENTER key to shutdown and quit VNM!
 docker stop vnm
 docker rm vnm
