7f0488ce445c357fb19eec12e53a03a7c5c76d51
Steffen <stebo85@web.de>
2020-06-24 15:53:01 +1000

    added -d to docker run command

--- build_run_stop.bat
+++ build_run_stop.bat
@@ -1,6 +1,6 @@
 docker build -t vnm:latest . && (
     ECHO "Starting VNM:"
-    REM docker run --privileged -e USER=neuro -e PASSWORD=neuro --name vnm -v C:/vnm:/vnm -p 6080:80 vnm:latest
+    REM docker run -d --privileged -e USER=neuro -e PASSWORD=neuro --name vnm -v C:/vnm:/vnm -p 6080:80 vnm:latest
     docker run -d --privileged -e USER=neuro -e PASSWORD=neuro --name vnm -v C:/vnm:/vnm -e RESOLUTION=1920x980 -p 6080:80 -p 5900:5900 vnm:latest
     set /p=VNM is running - press ENTER key to shutdown and quit VNM!
     docker stop vnm
