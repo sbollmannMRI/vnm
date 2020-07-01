d6227fb4d555003bb704a4a3c8a8a0d866e332ce
Thom Shaw <t.shaw@uq.edu.au>
2020-07-01 14:29:01 +1000

    include git clone vnm

--- start-here.bat
+++ start-here.bat
@@ -1,4 +1,4 @@
-rem First check if the VNC standalone is installed
+rem First check if the VNC standalone exists (standalone requires no installation! yay)
 
 if exist 'C:\vnm\RealVNCStandalone.exe' (
     echo "VNC standalone exists"
@@ -11,7 +11,8 @@ if exist 'C:\vnm\RealVNCStandalone.exe' (
     }
     powershell -Command "Invoke-WebRequest https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.20.529-Windows-64bit.exe -OutFile 'C:\vnm\RealVNCStandalone.exe'"
 )
-rem "checking for docker, none of this works yet"
+
+rem "check for docker, none of this works yet, the idea is to check for the docker, if it exists then keep going, if not, open the docker install web page with default browser"
 @echo off
 if "Get-Process 'com.docker.proxy' &&  echo $? " == "False" ( 
  echo "--------------------------------------------------------------"
@@ -21,20 +22,21 @@ echo "---------------------------------------------------------------"
 ) else (
 ::"I don't like to think of PowerShell as "CMD with the stupid parts removed". I like to think of it as "Bash without any of the useful bits".
 rem "build and start the docker image plus the VNC viewer"
-docker build -t vnm:latest . && (
-    ECHO "Starting VNM:"
-    docker images
-    REM docker run -d --privileged -e USER=neuro -e PASSWORD=neuro --name vnm -v C:/vnm:/vnm -p 6080:80 vnm:latest ## -p 6080:80
-docker run -d --privileged -e USER=neuro -e PASSWORD=neuro --name vnm -v C:/vnm:/vnm -e RESOLUTION=1920x990 -p 5900:5900 vnm:latest  
+git clone https://github.com/NeuroDesk/vnm.git C:\vnm\vnm-git
+docker build -t vnm:latest C:\vnm\vnm-git
+ECHO "Starting VNM:"
+rem docker images
+REM docker run -d --privileged -e USER=neuro -e PASSWORD=neuro --name vnm -v C:/vnm:/vnm -p 6080:80 vnm:latest ## -p 6080:80
+docker run -d --privileged -e USER=neuro -e PASSWORD=neuro --name vnm -v C:/vnm:/vnm -e RESOLUTION=1920x990 -p 5900:5900 vnm:latest 
 echo "docker started, opening VNC session"
-start 'C:\vnm\RealVNCStandalone.exe' localhost:5900 
-    rem remove the last session
-    set /p=VNM is running - press ENTER key to shutdown and quit VNM!
-    docker stop vnm
-    docker rm vnm
-)) || (
-    echo "-------------------------"
-    echo "Docker Build failed!"
-    echo "-------------------------"
+powershell -Command "start 'C:\vnm\RealVNCStandalone.exe' localhost:5900"
+
+set /p=VNM is running - press ENTER key to shutdown and quit VNM!
+docker stop vnm
+docker rm vnm
+) || (
+echo "-------------------------"
+echo "Docker Build failed!"
+echo "-------------------------"
 )
 )
