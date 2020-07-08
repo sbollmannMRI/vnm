45067598cb6ba7cb43fd0eabb6ebcb9704e85fb8
Dr Steffen Bollmann <uqsbollm@cai-arwana.cai.uq.edu.au>
2020-07-08 21:12:57 +1000

    modified menu to be in root and cleaned up old files

--- build_run_stop.sh
+++ build_run_stop.sh
@@ -2,7 +2,7 @@ sudo docker build -t vnm:latest . && (
     echo "Starting VNM:"
     sudo docker images
     sudo docker run -d --privileged --name vnm -v ~/vnm:/vnm -p 6080:80 -p 5900:5900 vnm:latest
-    read -p -e "VNM is running - press ENTER key to shutdown and quit VNM!"
+    read -e -p "VNM is running - press ENTER key to shutdown and quit VNM!" check
     sudo docker stop vnm
     sudo docker rm vnm
 ) || (
