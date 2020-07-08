3e3675f3ba88fecc0cf5be2bfaa975a1c708fbb7
Dr Steffen Bollmann <uqsbollm@cai-arwana.cai.uq.edu.au>
2020-07-08 20:57:12 +1000

    added build_run_stop script for linux development

---
+++ build_run_stop.sh
@@ -0,0 +1,12 @@
+sudo docker build -t vnm:latest . && (
+    echo "Starting VNM:"
+    sudo docker images
+    sudo docker run -d --privileged --name vnm -v ~/vnm:/vnm -p 6080:80 -p 5900:5900 vnm:latest
+    read -p -e "VNM is running - press ENTER key to shutdown and quit VNM!"
+    sudo docker stop vnm
+    sudo docker rm vnm
+) || (
+    echo "-------------------------"
+    echo "Docker Build failed!"
+    echo "-------------------------"
+)
